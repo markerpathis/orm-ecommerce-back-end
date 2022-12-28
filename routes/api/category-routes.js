145daa08c148b3a332737fd75fac5ab9967d33aa
Parker Mathis <parkerjmathis@gmail.com>
2022-12-28 14:15:55 -0800

    Added category and tag put routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -40,6 +40,20 @@ router.post("/", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: "No category found for this id" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
