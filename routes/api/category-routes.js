448f9ed8650976a995c6180dac34c40687a399e9
Parker Mathis <parkerjmathis@gmail.com>
2022-12-28 14:30:35 -0800

    Added delete routes for category, product and tag

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -58,6 +58,20 @@ router.put("/:id", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deletedCategory = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (deletedCategory) {
+      res.status(200).json(deletedCategory);
+    } else {
+      res.status(404).json({ message: "No category found with this id" });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -57,8 +57,22 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deletedTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (deletedTag) {
+      res.status(200).json(deletedTag);
+    } else {
+      res.status(404).json({ message: "No tag found with this id" });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
